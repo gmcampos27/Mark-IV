4c1303c52cb7fa2b1e3220bdd56a01381fd156c1
Gabriel de Campos <64294561+gmcampos27@users.noreply.github.com>
2022-11-07 10:47:54 -0300

    Update join_taxid.py

--- join_taxid.py
+++ join_taxid.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 
 
-# df = pd.read_excel('C:/Users/gabri/Dropbox/PC/Desktop/USP/Metagenômica/scaffolds_above_500_vs_nt_qcov70_uniq.xlsx')
+# df = pd.read_excel('file.xlsx')
 
-# taxIDs = pd.read_csv('C:/Users/gabri/Dropbox/PC/Desktop/USP/Metagenômica/tax_report.txt', sep = '\t')
+# taxIDs = pd.read_csv('tax.txt', sep = '\t')
 
 # print(taxIDs.columns)
 
@@ -16,9 +16,9 @@ import os
 
 # joined
 
-# join = joined.to_excel('C:/Users/gabri/Dropbox/PC/Desktop/USP/Metagenômica/pool9_a2_blast-scaffolds_taxID_data_nome.xlsx', index = False)
+# join = joined.to_excel('joined.xlsx', index = False)
 
-data = pd.read_excel('C:/Users/gabri/Dropbox/PC/Desktop/USP/Metagenômica/pool9_a2_blast-scaffolds_taxID_data_nome.xlsx')
+data = pd.read_excel('join.xlsx')
 print(data.columns)
 virus = data[data['taxname'] == 'Coxsackievirus A2']
 print(virus)
