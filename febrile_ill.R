dee987934caf2a515294d9b9ff0d40177543d969
Gabriel de Campos <64294561+gmcampos27@users.noreply.github.com>
2023-03-30 17:45:28 -0300

    Update febrile_ill.R

--- febrile_ill.R
+++ febrile_ill.R
@@ -1,55 +1,79 @@
-##Gabriel Montenegro de Campos
-##19-08-22
+#Diretório
+setwd('path')
 
-##Diretório
-
-setwd("path")
-
-##Bibliotecas
-library(viridis)
+#biblioteca
 library(readr)
+library(viridis)
+library(tidyverse)
+library(ggplot2)
+library(ggsci)
+library(wesanderson)
 
-##Dados
-All <- read_csv("file.csv")
-View(All)
-
-virus <- file$viruses
+#Dado
+data <- read_delim("DoençasFebris.csv", delim = ',')
+virus <- data$virus
+data <- replace(x = data, list = is.na(data), values = 0)
+pool <- colnames(data[2:14])
 
 ##Filtro, caso necessário
 
-mx <- as.matrix(All[-1]) ##tirou a coluna de vírus
+mx <- as.matrix(data[-1]) ##tirou a coluna de vírus
 rownames(mx) <- virus
-View(mx)
 
-##Barplot
 
 #cores
 
-c25 <- c(
+c39 <- c(
   "dodgerblue2", "#E31A1C", # red
   "green4",
   "#6A3D9A", # purple
   "#FF7F00", # orange
   "black", "gold1",
-  "skyblue2", "#FB9A99", # lt pink
+  "skyblue3", "#FB9A99", # lt pink
   "palegreen2",
   "#CAB2D6", # lt purple
   "#FDBF6F", # lt orange
   "gray70", "khaki2",
-  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
-  "darkturquoise", "green1", "yellow4", "yellow3",
-  "darkorange4", "brown"
+  "maroon", "orchid1", "deeppink1", "blue1", "red",
+  "darkturquoise", "grey", "yellow", "yellow3",
+  "darkorange4", "brown", "#3182BDFF", "#E6550DFF", "#739B58FF", "#3B1B53FF", "#6BD76BFF",
+  "#D3333B", "#5DB1DDFF", "#C75127FF", "#CC33FFFF", "#CCFFFFFF", "#66FFFF", "#00FF00FF",
+  "#E6A0C4", "#7294D4"
 )
 
+length(c39)
+col = pal_ucscgb("default")(19)
+library("scales")
+show_col(pal_ucscgb("default")(19))
 
-barplot(mx, col = c25, main = "Febrile Illnesses", xlab = "Pool", ylab = "Reads", ylim = c(0,8000))
-legend(x = 2,  y = 7000,
-       fill = c25,
-       border = 'black',
-       title = "Viruses",
-       legend = virus,
-       cex = 0.9
-)
+#Objetos
 
-##Gráficos usando o ggplot2
-print("thinking...")
+virus_name <- rep(virus, 13)
+
+pool_number <- rep(pool, each = 39)
+
+mylist <- list()
+
+for (i in 2:14){
+  join_virus <- data[1:39,i]
+  soma <- sum(data[1:39,i])
+  percent <-  (join_virus/soma)*100
+  mylist[[i]] <- percent
+}
+
+df <- data.frame(virus_name, pool_number)
+dftest <- data.frame(mylist[2:14])
+
+percent_df <- data.frame(Pool.1=unlist(dftest, use.names = F))
+join(df, percent_df)
+df <- data.frame(virus_name, pool_number, percent_df)
+
+df["percent"] <- percent_df$Pool.1
+
+data1 <- df
+data1$pool_number <- factor(data1$pool_number, levels = c("Pool 1", "Pool 2", "Pool 3", "Pool 4", "Pool 7", "Pool 8", "Pool 9", "Pool 10", "Pool 11", "Pool 12", "Pool 13", "Pool 14" ,"Pool 15"))
+
+p <- ggplot(data1, aes(x = pool_number, y = percent, fill = virus_name)) + 
+  geom_bar(stat="identity", width=0.5, colour="black") + scale_fill_manual(values = c39)
+p + ggtitle("Frebile Illness") + xlab("Pool") + ylab("Reads (%)") + labs(fill = "Viruses")
+
